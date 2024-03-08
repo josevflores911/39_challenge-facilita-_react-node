f3535b90e7b8151fc082f4750165c5dab46ccdef
josevflores911 <josevflores911@gmail.com>
2024-03-08 15:31:23 -0300

    updates

--- frontend/frontend/src/components/home/ClientSingleCard.jsx
+++ frontend/frontend/src/components/home/ClientSingleCard.jsx
@@ -15,7 +15,7 @@ const ClientSingleCard = ({ client }) => {
         <div key={client.id}
           className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
           <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
-            {client.telephone}
+            tlf: {client.telephone}
           </h2>
           <h4 className="my-2 text-gray-500">{client.id}</h4>
           <div className="flex justify-start items-center gap-x-2">
